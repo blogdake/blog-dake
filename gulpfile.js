e1d3f57522c9664c1cdbf4e0ccbc997c95fcee5f
blogdake <dauke_dake_98@mail.ru>
2019-12-02 18:39:10 +0500

    include style.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,43 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
 
-gulp.task('hello', (done) =>{
-  console.log('привет мир');
-  done();
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var minifyCSS = require('gulp-minify-css');
+
+// gulp.task('default', function() {
+//  gulp.src('css/style.css')
+//   .pipe(minifyCSS())
+//   .pipe(rename('style.min.css'))
+//   .pipe(gulp('app/'));
+
+
+// });
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/*.css')
+    .pipe(minifyCSS('style.css'))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css/"));
 });
 
-// Static server
-gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "./"
-      }
-  });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+
+// const gulp = require('gulp');
+// const browserSync = require('browser-sync').create();
+// gulp.task('hello', (done) =>{
+//   console.log('привет мир');
+  
+//   done();
+// });
+
+// // Static server
+// gulp.task('browser-sync', () => {
+//   browserSync.init({
+//       server: {
+//           baseDir: "./"
+//       }
+//   });
+//   gulp.watch("./*.css").on('change', browserSync.reload);
+//   gulp.watch("./*.html").on('change', browserSync.reload);
+// });
+
+
