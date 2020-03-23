95a8f1fcbdd939dc8e10e56fab241a1eb7b215fe
blogdake <57899713+blogdake@users.noreply.github.com>
2020-03-23 23:11:12 +0500

    Update control-send.php

--- control-send.php
+++ control-send.php
@@ -19,7 +19,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'k.dauke548@gmail.com';                     // SMTP username
-    $mail->Password   = 'K1a2s2h3a4';                               // SMTP password
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 465;                                    // TCP port to connect to
 
